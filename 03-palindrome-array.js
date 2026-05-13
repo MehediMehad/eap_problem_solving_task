ffde1b654f6e13f5cd9cc8750125d919099d5877
Mehedi Mehad <mdmehedihasanmehad@gmail.com>
2026-05-13 13:16:52 +0600

    feat: solution for palindrome array problem

---
+++ 03-palindrome-array.js
@@ -0,0 +1,17 @@
+function isPalindromeArray(arr) {
+  let left = 0;
+  let right = arr.length - 1;
+
+  while (left < right) {
+    if (arr[left] !== arr[right]) {
+      return false;
+    }
+    left++;
+    right--;
+  }
+
+  return true;
+}
+
+console.log(isPalindromeArray([1, 2, 3, 2, 1])); // true
+console.log(isPalindromeArray([1, 2, 3])); // false
